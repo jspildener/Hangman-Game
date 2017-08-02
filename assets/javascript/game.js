ab47ede1f939040005dce52d052ebc39f8386cb6
William R Poulson <jessica.spildener@gmail.com>
2017-08-02 19:58:27 -0400

    javascript updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,11 @@
+
+// ****GAME SETUP******
+
 var wins = 0;
-var guessesRemaining = 12;
+var guessesRemaining = 15;
 var lettersGuessed = [];
-var words = ["beaker", "erlenmeyer flask", "periodic table", "deoxyribonucleic acid",
-    "hypothesis", "scientific method"
+var words = ["beaker", "flask", "periodic table", "deoxyribonucleic acid",
+    "hypothesis", "scientific method", "science"
 ];
 
 var generateWordChoice = function() {
@@ -10,27 +13,42 @@ var generateWordChoice = function() {
 };
 var currentWord = generateWordChoice();
 var letterCount = currentWord.length;
-var displayWord = [];
+var lettersGuessedCorrectly = [];
+var displayWord = "";
 for (i = 0; i < letterCount; i++) {
-    displayWord.push("_");
+    lettersGuessedCorrectly.push("_ ");
+    displayWord += "_ ";
 }
 
+document.getElementById("currentWord").innerHTML = displayWord;
+document.getElementById("wins").innerHTML = wins;
+document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
+
+
+//*****GAME PLAY*****
+
+
+console.log(currentWord)
 document.onkeyup = function(event) {
-        var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    guessesRemaining--;
+    document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
+    if (currentWord.indexOf(userGuess) > -1) {
+        lettersGuessed.push(userGuess);
+        displayWord = "";
+        for (i = 0; i < letterCount; i++) {
+            var currentLetter = currentWord[i];
+            if (userGuess === currentLetter) {
+                lettersGuessedCorrectly[i] = userGuess;
+                console.log("current user guess: " + lettersGuessedCorrectly[i]);
 
-        if (currentWord.indexOf(userGuess) > -1) {
-            guessesRemaining--;
-            document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
-            lettersGuessed.push(userGuess);
-            for (i = 0; i < letterCount; i++) {
-            	var currentLetter = currentWord[i];
-                if (userGuess === currentLetter) {
-                	displayWord[i] = userGuess;
-                }
             }
-
-
-
-
-
+            displayWord = displayWord + lettersGuessedCorrectly[i];
+            document.getElementById("currentWord").innerHTML = displayWord;
         }
+
+
+    } else {
+        console.log("WRONG");
+    }
+};
